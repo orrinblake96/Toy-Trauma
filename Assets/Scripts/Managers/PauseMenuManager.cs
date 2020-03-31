a03056ae79e6862e2e340ae1365aa402f23ce2d4
OrrinBlake <orrinblake@gmail.com>
2020-03-31 01:14:45 +0100

    Add Start/Pause menus

---
+++ Assets/Scripts/Managers/PauseMenuManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Player;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class PauseMenuManager : MonoBehaviour
+    {
+        public static bool gameIsPaused = false;
+
+        public GameObject pauseMenuUi;
+        public GameObject playerShooting;
+
+        private void Update()
+        {
+            if (!Input.GetKeyDown(KeyCode.Escape)) return;
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+        public void Resume()
+        {
+            playerShooting.SetActive(true);
+            Time.timeScale = 1.0f;
+            pauseMenuUi.SetActive(false);
+            gameIsPaused = false; 
+        }
+
+        private void Pause()
+        {
+            playerShooting.SetActive(false);
+            Time.timeScale = 0.0f;
+            pauseMenuUi.SetActive(true);
+            gameIsPaused = true;
+        }
+
+        public void LoadMenu()
+        {
+            SceneManager.LoadScene("StartMenu");
+            Time.timeScale = 1.0f;
+            playerShooting.SetActive(true);
+        }
+
+        public void QuitGame()
+        {
+            SceneManager.LoadScene("StartMenu");
+            Time.timeScale = 1.0f;
+            playerShooting.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/StartMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class StartMenuManager : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Credits Rolling");
+        }
+    }
+}
