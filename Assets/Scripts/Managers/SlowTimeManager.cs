cafcf6e0e3cbdbed6b02e6fb876d2ae31d927d21
OrrinBlake <orrinblake@gmail.com>
2020-04-20 17:15:25 +0100

    Time slowed
    — Need to slow pitch also

---
+++ Assets/Scripts/Managers/SlowTimeManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Managers
+{
+    public class SlowTimeManager : MonoBehaviour
+    {
+        public float slowdownFactor = 0.05f;
+        public float slowdownLength = 2f;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                SlowMotion();
+            }
+
+            Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
+            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        }
+
+        void SlowMotion()
+        {
+            Time.timeScale = slowdownFactor;
+            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        }
+    }
+}
